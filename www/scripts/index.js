489ec6106e6e3af5e7f6608e8fab9f5bfde756da
Rémy Pottiez <rpottiez.pro@gmail.com>
2020-12-20 17:28:25 +0100

    web

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -14,8 +14,6 @@ window.onload = function(){
         maxZoom: 20
     }).addTo(macarte)
 
-    document.querySelector('#loadChart').addEventListener('click', Chart);
-
     // Cette méthode est à insérer juste après avoir initialisé la carte
     var routing = L.Routing.control({
         // Nous personnalisons le tracé
@@ -26,21 +24,24 @@ window.onload = function(){
         // Nous personnalisons la langue et le moyen de transport
         router: new L.Routing.osrmv1({
             language: 'fr',
-            profile: 'bike', // car, bike, foot
+            profile: 'car', // car, bike, foot
         }),
     
         geocoder: L.Control.Geocoder.nominatim()
     }).addTo(macarte)
 
+    
     routing.on("routesfound", function(e) {
-        var tab = e.waypoints;
+        /*var tab = e.waypoints;
+        console.log(tab);
         for (let i = 0; i < tab.length; i++) {
             var destination = tab[i].name.split(',');
             tabDestination[i] = destination[0];
-        }
-        console.log(tabDestination);
+        }*/
+        Altitude();
     });
 
+    
 }
 
 function Chart(){
@@ -81,4 +82,32 @@ function Chart(){
             }
         }
     });
+}
+
+function Altitude(){
+    //all used options are the default values
+    var controlElevation = L.control.elevation({
+        position: "topright",
+        theme: "steelblue-theme", //default: lime-theme
+        width: 600,
+        height: 125,
+        margins: {
+            top: 10,
+            right: 20,
+            bottom: 30,
+            left: 50
+        },
+        useHeightIndicator: true, //if false a marker is drawn at map position
+        interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
+        hoverNumber: {
+            decimalsX: 3, //decimals on distance (always in km)
+            decimalsY: 0, //deciamls on height (always in m)
+            formatter: undefined //custom formatter function may be injected
+        },
+        xTicks: undefined, //number of ticks in x axis, calculated by default according to width
+        yTicks: undefined, //number of ticks on y axis, calculated by default according to height
+        collapsed: false	//collapsed mode, show chart on click or mouseover
+    });
+    controlElevation.addData.bind(controlElevation); //working on a better solution
+    controlElevation.addTo(macarte);
 }
