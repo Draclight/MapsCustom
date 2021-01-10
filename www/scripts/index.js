a373865cf514f4bc2b6fb5e1a7faaf050abc99a2
Rémy Pottiez <rpottiez.pro@gmail.com>
2021-01-10 01:03:13 +0100

    Maps

--- www/scripts/index.js
+++ www/scripts/index.js
@@ -1,113 +1,146 @@
-let macarte, marqueur;
-var tabDestination = [];
-var tabAlt = [];
 
-// On s'assure que la page est chargée
-window.onload = function(){
-    // On initialise la carte sur les coordonnées GPS de Paris
-    macarte = L.map('carte').setView([48.852969, 2.349903], 8)
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHJhY2xpZ2h0IiwiYSI6ImNramswY2FiaDl5N2oycWxia2N5cmNxc3IifQ.yQMl4nh6wdmh75dHTyP3pg';
 
-    // On charge les tuiles depuis un serveur au choix, ici OpenStreetMap France
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-        attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-        minZoom: 1,
-        maxZoom: 20
-    }).addTo(macarte)
+var tabElevation = [];
+var tabPoint = [];
 
-    // Cette méthode est à insérer juste après avoir initialisé la carte
-    var routing = L.Routing.control({
-        // Nous personnalisons le tracé
-        lineOptions: {
-            styles: [{color: '#ff8f00', opacity: 1, weight: 7}]
-        },
-    
-        // Nous personnalisons la langue et le moyen de transport
-        router: new L.Routing.osrmv1({
-            language: 'fr',
-            profile: 'car', // car, bike, foot
-        }),
-    
-        geocoder: L.Control.Geocoder.nominatim()
-    }).addTo(macarte)
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+    enableHighAccuracy: true
+});
 
-    
-    routing.on("routesfound", function(e) {
-        /*var tab = e.waypoints;
-        console.log(tab);
-        for (let i = 0; i < tab.length; i++) {
-            var destination = tab[i].name.split(',');
-            tabDestination[i] = destination[0];
-        }*/
-        Altitude();
-    });
-
-    
+function successLocation(position) {
+    setupMap([position.coords.longitude, position.coords.latitude]);
 }
 
-function Chart(){
-    var ctx = document.getElementById('myChart');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: tabDestination,
-            datasets: [{
-                label: 'Altitude',
-                data: [2, 5],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
+function errorLocation(){
+    setupMap([2.295753,49.894067])
+}
+
+function setupMap(center){
+    const map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/streets-v11",
+        center, center,
+        zoom: 13
+    });
+
+    const nav = new mapboxgl.NavigationControl();
+    map.addControl(nav);
+
+    var directions = new MapboxDirections({
+        accessToken: mapboxgl.accessToken
+    });
+
+    map.addControl(directions, "top-left");
+
+    directions.on('route', function(e){
+
+        tabElevation = [];
+        tabPoint = [];
+
+        var route = e.route[0];
+        var leg = route.legs[0];
+        var steps = leg.steps;
+
+        steps.forEach(function (step, i) {
+            
+            var query = 'https://api.mapbox.com/v4/mapbox.mapbox-terrain-v2/tilequery/' + location[0] + ',' + location[1] + '.json?layers=contour&limit=50&access_token=' + mapboxgl.accessToken;
+            $.ajax({
+                method: 'GET',
+                url: query,
+            }).done(function(data) {
+                // Get all the returned features
+                var allFeatures = data.features;
+                // Create an empty array to add elevation data to
+                var elevations = [];
+                // For each returned feature, add elevation data to the elevations array
+                for (i = 0; i < allFeatures.length; i++) {
+                    elevations.push(allFeatures[i].properties.ele);
+                }
+                // In the elevations array, find the largest value
+                highestElevation = Math.max(...elevations);
+                // Display the largest elevation value
+                tabElevation.push(highestElevation);
+                tabPoint.push(step.name);
+            });            
+        });
+    });
+}
+
+function getElevation(lng, lat) {
+    // make API request
+    var query = 'https://api.mapbox.com/v4/mapbox.mapbox-terrain-v2/tilequery/' + lng + ',' + lat + '.json?layers=contour&limit=50&access_token=' + mapboxgl.accessToken;
+    $.ajax({
+        method: 'GET',
+        url: query,
+    }).done(function(data) {
+        // Get all the returned features
+        var allFeatures = data.features;
+        // Create an empty array to add elevation data to
+        var elevations = [];
+        // For each returned feature, add elevation data to the elevations array
+        for (i = 0; i < allFeatures.length; i++) {
+            elevations.push(allFeatures[i].properties.ele);
         }
+        // In the elevations array, find the largest value
+        highestElevation = Math.max(...elevations);
+        // Display the largest elevation value
+        return parseFloat(highestElevation);
     });
 }
 
-function Altitude(){
-    //all used options are the default values
-    var controlElevation = L.control.elevation({
-        position: "topright",
-        theme: "steelblue-theme", //default: lime-theme
-        width: 600,
-        height: 125,
-        margins: {
-            top: 10,
-            right: 20,
-            bottom: 30,
-            left: 50
-        },
-        useHeightIndicator: true, //if false a marker is drawn at map position
-        interpolation: "linear", //see https://github.com/mbostock/d3/wiki/SVG-Shapes#wiki-area_interpolate
-        hoverNumber: {
-            decimalsX: 3, //decimals on distance (always in km)
-            decimalsY: 0, //deciamls on height (always in m)
-            formatter: undefined //custom formatter function may be injected
-        },
-        xTicks: undefined, //number of ticks in x axis, calculated by default according to width
-        yTicks: undefined, //number of ticks on y axis, calculated by default according to height
-        collapsed: false	//collapsed mode, show chart on click or mouseover
-    });
-    controlElevation.addData.bind(controlElevation); //working on a better solution
-    controlElevation.addTo(macarte);
+function clearCanvas(){
+    var canvas = document.getElementById('myChart');
+    var context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function affichage(){
+
+        clearCanvas()
+
+        var canvas = document.getElementById('myChart');
+        var myChart = new Chart(canvas, {
+            type: 'line',
+            data: {
+                labels: tabPoint,
+                datasets: [{
+                    label: 'Altitude by ft',
+                    data: tabElevation,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
+        document.getElementById('canvas').hidden = false;
+}
+
+function myFunction() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
